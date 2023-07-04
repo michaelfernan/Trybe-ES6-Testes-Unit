eb23b3ced515570da5eecfc55a89c86184c7fb8d
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-04 12:26:46 -0300

    requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,24 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const sum = number1 + number2;
+  const mult = number1 * number2;
+  const div = Math.floor(number1 / number2);
+  const sub = number1 - number2;
 
-const arrayGenerator = (type, object) => {};
+  return { sum, mult, div, sub };
+};
+
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+  } if (type === 'values') {
+    return Object.values(object);
+  } if (type === 'entries') {
+    return Object.entries(object);
+  } 
+    throw new Error('Invalid type. Use "keys", "values" or "entries".');
+};
 
 module.exports = { calculator, arrayGenerator };
