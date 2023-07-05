22fc5a9291698a8f0fc8112249ace3f7281fc529
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-05 09:45:13 -0300

    requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,7 +23,33 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
+  });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número', () => {
+        expect(circle('5')).toBeUndefined();
+      });
+    
+      it('Teste se circle retorna um objeto', () => {
+        expect(typeof circle(5)).toBe('object');
+      });
+    
+      it('Teste se o objeto retornado possui 3 propriedades', () => {
+        const circleObject = circle(5);
+        expect(Object.keys(circleObject).length).toBe(3);
+      });
+    
+      it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined', () => {
+        expect(circle()).toBeUndefined();
+      });
+    
+      it('Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2', () => {
+        const circleObject = circle(2);
+        expect(circleObject.circumference).toBeCloseTo(12.56, 2);
+      });
+    
+      it('Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3', () => {
+        const circleObject = circle(3);
+        expect(circleObject).toEqual({ radius: 3, area: 28.259999999999998, circumference: 18.84 });
+      });
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     // Teste se circle retorna um objeto.
@@ -33,4 +59,4 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
   });
-});
+
